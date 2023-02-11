1006a8451869ad837877081a442d739e69adcd17
RcleydsonR <rafael.cleydson@gmail.com>
2023-02-11 20:25:46 -0300

    Adicionando conexao ao mqtt e enviando mensagem quando ocorre leitura do sensor de temperatura

--- main/main.c
+++ main/main.c
@@ -8,29 +8,33 @@
 #include "temperature_sensor.h"
 #include "i2c.h"
 #include "lcd.h"
+#include "mqtt.h"
+#include "wifi.h"
 
 #define TAG "MAIN"
 
 void app_main(void)
 {
 #if CONFIG_ESP_MODE_SENDER
-    // xTaskCreate(&read_temperature, "Temperatura DHT11", 2048, NULL, 1, NULL);
     init_components();
 
-    while (1) {
-        lcd_clear();
-        lcd_put_cur(0, 0);
-        lcd_send_string("ASfascuinsc", 100);
+    xTaskCreate(&read_temperature, "Temperatura DHT11", 2048, NULL, 1, NULL);
+    xTaskCreate(&wifi_connected, "Conexao wifi", 2048, NULL, 1, NULL);
 
-        lcd_put_cur(1, 0);
-        lcd_send_string("afinasc assac", 100);
+    // while (1) {
+    //     lcd_clear();
+    //     lcd_put_cur(0, 0);
+    //     lcd_send_string("ASfascuinsc", 100);
 
-        lcd_clear();
-        lcd_put_cur(0, 0);
-        lcd_send_string("gsdinasc dgsv", 100);
+    //     lcd_put_cur(1, 0);
+    //     lcd_send_string("afinasc assac", 100);
 
-        lcd_put_cur(1, 0);
-        lcd_send_string("oidnvs aocpm asc", 100);
-    }
+    //     lcd_clear();
+    //     lcd_put_cur(0, 0);
+    //     lcd_send_string("gsdinasc dgsv", 100);
+
+    //     lcd_put_cur(1, 0);
+    //     lcd_send_string("oidnvs aocpm asc", 100);
+    // }
 #endif
 }
--- main/temperature_sensor.c
+++ main/temperature_sensor.c
@@ -8,18 +8,26 @@
 
 #include "dht11.h"
 #include "temperature_sensor.h"
+#include "mqtt.h"
+#include "init.h"
 
 #define TAG "DHT11"
 
 void read_temperature(void *params)
 {
     struct dht11_reading dht;
+    char message[80];
+
     while (1)
     {
         dht = DHT11_read();
 
-        if (dht.status == DHT11_OK)
-            ESP_LOGI(TAG, "Temperatura: %d, Humidade: %d", dht.temperature, dht.humidity);
+        if (dht.status == DHT11_OK) {
+            sprintf(message, "Temperatura: %d | Humidade: %d", dht.temperature, dht.humidity);
+            ESP_LOGI(TAG, "Temperatura: %d | Humidade: %d", dht.temperature, dht.humidity);
+            if(mqttSemaphoreConn)
+                mqtt_send_message("JoaoFlavioRafael", message);
+        }
         else
             ESP_LOGE(TAG, "Erro na leitura do sensor de temperatura");
 
